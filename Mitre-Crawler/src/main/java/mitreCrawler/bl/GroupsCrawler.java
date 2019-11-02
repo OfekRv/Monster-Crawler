9a782d741c591f870c5adb3a22680576dd5ed33e
OfekRv <ofekRv@gmail.com>
2019-11-02 23:13:55 +0200

    logged messages

--- Mitre-Crawler/src/main/java/mitreCrawler/bl/GroupsCrawler.java
+++ Mitre-Crawler/src/main/java/mitreCrawler/bl/GroupsCrawler.java
@@ -69,6 +69,9 @@ public class GroupsCrawler implements Crawler<Group> {
 
 			if (changeLog.contanisChange()) {
 				changeLogRepository.save(changeLog);
+				log.info("change log saved");
+			} else {
+				log.info("no changes detected");
 			}
 
 		} catch (IOException e) {
