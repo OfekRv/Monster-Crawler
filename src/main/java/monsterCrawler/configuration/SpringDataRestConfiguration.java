ef26dfdeaa6f47b42192830982da68e21feddb7c
OfekRv <ofekRv@gmail.com>
2019-11-16 21:49:14 +0200

    Change log removed

--- src/main/java/monsterCrawler/configuration/SpringDataRestConfiguration.java
+++ src/main/java/monsterCrawler/configuration/SpringDataRestConfiguration.java
@@ -5,7 +5,6 @@ import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
 
 import monsterCrawler.entities.Article;
-import monsterCrawler.entities.ChangeLog;
 import monsterCrawler.entities.Group;
 import monsterCrawler.entities.Software;
 import monsterCrawler.entities.Technique;
@@ -17,7 +16,6 @@ public class SpringDataRestConfiguration extends RepositoryRestConfigurerAdapter
 		config.exposeIdsFor(Group.class);
 		config.exposeIdsFor(Technique.class);
 		config.exposeIdsFor(Software.class);
-		config.exposeIdsFor(ChangeLog.class);
 		config.exposeIdsFor(Article.class);
 	}
 }
