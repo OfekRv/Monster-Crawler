0449df4efee25e6a5bd8b791297f19925bc930c4
OfekRv <ofekRv@gmail.com>
2019-12-06 09:20:51 +0200

    united crawlers

--- src/main/java/monsterCrawler/bl/crawlers/CrawlersManager.java
+++ src/main/java/monsterCrawler/bl/crawlers/CrawlersManager.java
@@ -5,8 +5,6 @@ import java.util.Collection;
 import javax.inject.Inject;
 import javax.inject.Named;
 
-import org.springframework.beans.factory.annotation.Value;
-
 import lombok.extern.slf4j.Slf4j;
 import monsterCrawler.bl.crawlers.articles.ArticlesCrawler;
 import monsterCrawler.bl.crawlers.groups.AttackGroupsCrawler;
@@ -23,13 +21,10 @@ public class CrawlersManager {
 	@Inject
 	private GroupRepository groupRepository;
 
-	@Value("${CRAWL_GROUPS}")
-	private boolean isCrawlGroup;
-
 	public void crawl() {
-		if (isCrawlGroup) {
-			crawlGroups();
-		}
+		log.info("Started executing groups crawler");
+		groupsCrawler.crawl();
+		log.info("Finished executing groups crawler");
 
 		log.info("Started executing atricles crawler");
 		for (Group group : groupRepository.findAll()) {
@@ -39,11 +34,5 @@ public class CrawlersManager {
 			}
 		}
 		log.info("Finished executing articles crawler");
-	}
-
-	public void crawlGroups() {
-		log.info("Started executing groups crawler");
-		groupsCrawler.crawl();
-		log.info("Finished executing groups crawler");
 	}
 }
