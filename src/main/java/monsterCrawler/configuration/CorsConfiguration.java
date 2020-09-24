f06a7ee91f92882d7087f509443c7320c027cc37
ofekrv <ofekrv@gmail.com>
2020-09-24 12:54:06 +0300

    add cors

--- src/main/java/monsterCrawler/configuration/CorsConfiguration.java
+++ src/main/java/monsterCrawler/configuration/CorsConfiguration.java
@@ -13,7 +13,7 @@ public class CorsConfiguration{
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**");
+                registry.addMapping("/**").allowedOrigins("*");
             }
         };
     }
