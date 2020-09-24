bb37fee294dc4c91df9bab9266fe0333a162a49d
ofekrv <ofekrv@gmail.com>
2020-09-24 21:34:22 +0300

    adding default header

---
+++ src/main/java/monsterCrawler/configuration/HeaderConfiguration.java
@@ -0,0 +1,21 @@
+package monsterCrawler.configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+@Component
+public class HeaderConfiguration extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest,
+                                    HttpServletResponse httpServletResponse,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+        httpServletResponse.addHeader("Access-Control-Allow-Origin", "*");
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
+    }
+}
