83f8436931a7a2a65de73ea85f6f65ed37396bc1
ofekr <ofekrv@gmail.com>
2021-01-11 16:41:20 +0200

    check if already exists by title to avoid duplications

--- src/main/java/monsterCrawler/bl/crawlers/articles/GbhackersArticleCrawler.java
+++ src/main/java/monsterCrawler/bl/crawlers/articles/GbhackersArticleCrawler.java
@@ -13,49 +13,50 @@ import static monsterCrawler.utils.CrawelersUtils.getFirstElementByClass;
 
 @Named
 public class GbhackersArticleCrawler extends AbstractArticlesCrawler<Group> implements GroupArticlesCrawler {
-	private static final String SEARCH_QUERY = "?s=";
-	private static final String PAGE = "page/";
+    private static final String SEARCH_QUERY = "?s=";
+    private static final String PAGE = "page/";
 
-	@Value("${GBHACKERS_URL}")
-	private String gbhackersUrl;
+    @Value("${GBHACKERS_URL}")
+    private String gbhackersUrl;
 
-	@Override
-	public String buildUrl(String name) {
-		return gbhackersUrl + SEARCH_QUERY + '"' + encodeUrl(name) + '"';
-	}
+    @Override
+    public String buildUrl(String name) {
+        return gbhackersUrl + SEARCH_QUERY + '"' + encodeUrl(name) + '"';
+    }
 
-	@Override
-	public String buildSearchUrl(String name, int currentPage) {
-		return gbhackersUrl + PAGE + currentPage + "/" + SEARCH_QUERY + '"' + encodeUrl(name) + '"';
-	}
+    @Override
+    public String buildSearchUrl(String name, int currentPage) {
+        return gbhackersUrl + PAGE + currentPage + "/" + SEARCH_QUERY + '"' + encodeUrl(name) + '"';
+    }
 
-	@Override
-	public String extractTitle(Element article) {
-		return article.selectFirst("h3").text();
-	}
+    @Override
+    public String extractTitle(Element article) {
+        return article.selectFirst("h3").text();
+    }
 
-	@Override
-	public Elements extractArticlesElements(Document doc) {
-		Element articlesContainer= getFirstElementByClass(doc,"td-ss-main-content");
-		if (articlesContainer==null)
-		{
-			return new Elements();
-		}
-		return articlesContainer.getElementsByClass("td_module_10 td_module_wrap td-animation-stack");
-	}
+    @Override
+    public Elements extractArticlesElements(Document doc) {
+        Element articlesContainer = getFirstElementByClass(doc, "td-ss-main-content");
 
-	@Override
-	public String extractArticleDate(Element article) {
-		return article.selectFirst("time").text();
-	}
+        if (articlesContainer == null) {
+            return new Elements();
+        }
 
-	@Override
-	public String getDateFormatPattern() {
-		return "MMMM d, yyyy";
-	}
+        return articlesContainer.getElementsByClass("td_module_10 td_module_wrap td-animation-stack");
+    }
 
-	@Override
-	public int getFirstSearchPageIndex() {
-		return 2;
-	}
+    @Override
+    public String extractArticleDate(Element article) {
+        return article.selectFirst("time").text();
+    }
+
+    @Override
+    public String getDateFormatPattern() {
+        return "MMMM d, yyyy";
+    }
+
+    @Override
+    public int getFirstSearchPageIndex() {
+        return 2;
+    }
 }
--- src/main/java/monsterCrawler/repositories/ArticleRepository.java
+++ src/main/java/monsterCrawler/repositories/ArticleRepository.java
@@ -1,21 +1,22 @@
 package monsterCrawler.repositories;
 
-import java.time.LocalDate;
-import java.util.Collection;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
 import monsterCrawler.entities.Article;
 import monsterCrawler.entities.Group;
 import monsterCrawler.entities.projections.ArticleProjection;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.time.LocalDate;
+import java.util.Collection;
 
 @RepositoryRestResource(excerptProjection = ArticleProjection.class)
 public interface ArticleRepository extends JpaRepository<Article, Integer> {
-	public boolean existsByUrl(String url);
+    public boolean existsByUrl(String url);
 
-	public Article findByUrl(String url);
+    public boolean existsByTitle(String title);
 
-	public Collection<Article> findAllByDateBetweenAndGroupsContaining(LocalDate startDate, LocalDate endDate,
-			Group group);
+    public Article findByUrl(String url);
+
+    public Collection<Article> findAllByDateBetweenAndGroupsContaining(LocalDate startDate, LocalDate endDate,
+                                                                       Group group);
 }
